8f2ad6d209fbddde2972058a6f98f9b029677505
Romina Hemati <romi.hemmati.kh.02@gmail.com>
2025-01-29 21:45:54 +0330

    add Registration & Account verification through email

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
-from account.views import Login
+from account.views import Login, Register, activate
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
     path('', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
     path('accounts/', include('account.urls')),
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
@@ -64,3 +65,51 @@ class Login(LoginView):
             return reverse_lazy("account:home")
         else:
             return reverse_lazy("account:profile")
+
+
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+
+
+class Register(CreateView):
+    form_class = SignupForm
+    template_name = "registration/register.html"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        current_site = get_current_site(self.request)
+        mail_subject = 'Activate your Django Blog account.'
+        message = render_to_string('registration/account_activation_email.html', context={
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        to_email = form.cleaned_data.get('email')
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
+        return render(self.request, 'registration/account_activation_done.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        is_done = True
+    else:
+        is_done = False
+    return render(request, 'registration/account_activation_complete.html', context={'is_done': is_done})
