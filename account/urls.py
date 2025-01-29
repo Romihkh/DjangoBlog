c236aa19c7ba2a85198678b48b2eedc6f384dab0
Romina Hemati <romi.hemmati.kh.02@gmail.com>
2025-01-29 21:02:10 +0330

    add DeleteArticle to AdminPanel

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ArticleList, ArticleCreate, ArticleUpdate
+from .views import ArticleList, ArticleCreate, ArticleUpdate, ArticleDelete
 
 app_name = 'account'
 
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
     path('article/new', ArticleCreate.as_view(), name='article-create'),
     path('article/edit/<int:pk>', ArticleUpdate.as_view(), name='article-update'),
-
+    path('article/delete/<int:pk>', ArticleDelete.as_view(), name='article-delete'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from blog.models import Article
-from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin
+from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin, DeletionMixin
 
 
 # Create your views here.
@@ -25,3 +26,9 @@ class ArticleCreate(LoginRequiredMixin, FieldsMixin, FormValidMixin, CreateView)
 class ArticleUpdate(AuthorAccessMixin, FieldsMixin, FormValidMixin, UpdateView):
     model = Article
     template_name = "registration/article-create-update.html"
+
+
+class ArticleDelete(DeletionMixin, DeleteView):
+    model = Article
+    success_url = reverse_lazy('account:home')
+    template_name = "registration/article_confirm_delete.html"
