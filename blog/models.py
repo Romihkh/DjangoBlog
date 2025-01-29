4d3d3fc9e114423a772a4e748ac5d9b74acc58dd
Romina Hemati <romi.hemmati.kh.02@gmail.com>
2025-01-29 14:58:06 +0330

    Create & Register blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.urls import reverse
+from account.models import User
+from django.utils.html import format_html
+from django.utils import timezone
+
+
+# my managers
+class ArticleManager(models.Manager):
+    def published(self):
+        return self.filter(status='p')
+
+
+class CategoryManager(models.Manager):
+    def active(self):
+        return self.filter(status=True)
+
 
 # Create your models here.
+class IPAddress(models.Model):
+    ip_address = models.GenericIPAddressField()
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.SET_NULL,
+                               related_name='children')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    status = models.BooleanField(default=True)
+    position = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['parent__id', 'position']
+
+    def __str__(self):
+        return self.title
+
+    objects = CategoryManager()
+
+
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),  # draft
+        ('p', "Publish"),  # publish
+        ('r', "Reviewing"),  # investigation
+        ('b', "Denied"),  # back
+    )
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='articles')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    category = models.ManyToManyField(Category, related_name="articles")
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to="images")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_special = models.BooleanField(default=False)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    hits = models.ManyToManyField(IPAddress, through="ArticleHit", blank=True, related_name="hits")
+
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("account:home")
+
+    def thumbnail_tag(self):
+        return format_html("<img width=100 height=75 style='border-radius: 5px;' src='{}'>".format(self.thumbnail.url))
+
+    thumbnail_tag.short_description = "thumbnail"
+
+    def category_to_str(self):
+        return "، ".join([category.title for category in self.category.active()])
+
+    category_to_str.short_description = "Category"
+
+    objects = ArticleManager()
+
+
+class ArticleHit(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    ip_address = models.ForeignKey(IPAddress, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
