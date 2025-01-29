ac500c13b8ef4f2099fd2574535a6131e38baf99
Romina Hemati <romi.hemmati.kh.02@gmail.com>
2025-01-29 21:14:22 +0330

    add Logout button to AdminPanel

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views
 from django.urls import path
 
 from .views import (ArticleList,
@@ -12,6 +13,13 @@ app_name = 'account'
 urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
     path('login/', Login.as_view(), name='login'),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    # path("password_change/", views.PasswordChangeView.as_view(), name="password_change" ),
+    # path("password_change/done/", views.PasswordChangeDoneView.as_view(), name="password_change_done", ),
+    # path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    # path("password_reset/done/", views.PasswordResetDoneView.as_view(), name="password_reset_done", ),
+    # path("reset/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(), name="password_reset_confirm", ),
+    # path("reset/done/", views.PasswordResetCompleteView.as_view(), name="password_reset_complete", ),
     path('article/new', ArticleCreate.as_view(), name='article-create'),
     path('article/edit/<int:pk>', ArticleUpdate.as_view(), name='article-update'),
     path('article/delete/<int:pk>', ArticleDelete.as_view(), name='article-delete'),
